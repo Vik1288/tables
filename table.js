25fd7a05adecdd853e1a4b19681880dda1de5608
Sachin Shukla <Sachinkumar05282@gmail.com>
2021-11-28 13:52:37 +0530

    ssa

---
+++ table.js
@@ -0,0 +1,6 @@
+for (var table = 1; table <= 20; table++) {
+    for (var i = 1; i <= 10; i++) {
+    console.log(table + " * " + (i) + " = " + (table * (i)));
+    }
+    console.log("----------");
+    }
